123a040633aafc622523075ea013d4e1d290d881
Collections <collections@bnbmusic.local>
2019-04-17 13:09:36 -0400

    added theme php helpers, enqueued scripts

---
+++ functions/dependencies.php
@@ -0,0 +1,17 @@
+<?php 
+
+//LOAD SCRIPTS
+function enqueue_craving_scripts() {
+    wp_enqueue_script('Popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', false, null, true, null);
+    wp_enqueue_script('Bootstrap-4.3.1', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array('jquery'), null, true, null);
+    wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/releases/v5.6.3/js/all.js', false, null, null, false);
+    wp_enqueue_script('rockshop-scripts', get_template_directory_uri() . '/assets/js/rockshopScripts.js', array('jquery'), null, true, null);
+}
+add_action('wp_enqueue_scripts', 'enqueue_craving_scripts');
+
+//LOAD CSS
+function enqueue_craving_styles() {
+    wp_enqueue_style('bootstrap-4.3.1', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), null);
+    wp_enqueue_style('rockshop', get_template_directory_uri() . '/assets/styles/rockshopStyles.css', array(), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
+}
+add_action('wp_enqueue_scripts', 'enqueue_craving_styles');
