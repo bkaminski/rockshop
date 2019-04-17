dfd0047e5aafdb5bb8cc12986c7a8b1545334bb4
Collections <collections@bnbmusic.local>
2019-04-17 16:16:21 -0400

    created navbar part

--- header.php
+++ header.php
@@ -25,14 +25,6 @@
 </head>
 <body <?php body_class(); ?>>
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="masthead mb-auto">
-            <div class="inner">
-                <h3 class="masthead-brand"><?php echo get_bloginfo('name');?></h3>
-                <nav class="nav nav-masthead justify-content-center">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Contact</a>
-                </nav>
-            </div>
-        </header>
+        <?php get_template_part( 'parts/home', 'nav' ); ?>
+        
   
