e46ac12757d7ed2ef08de4823526ef527a9fd845
Collections <collections@bnbmusic.local>
2019-04-17 12:02:01 -0400

    initial commit of WP theme files

---
+++ assets/styles/scss/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "../styles"
+sass_dir = "./"
+images_dir = "../img"
+javascripts_dir = "../js"
+environment = :development
+relative_assets = true
+output_style = :expanded
+preferred_syntax = :scss
