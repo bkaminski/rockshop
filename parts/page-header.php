6fa4afd4740fb09bd385c8912b2312959331be7a
Ben Kaminski <ben@benkaminski.com>
2019-04-20 12:35:13 -0400

    added video to header

--- parts/page-header.php
+++ parts/page-header.php
@@ -1,4 +1,12 @@
-<header class="container-lg header-contain">
-	
+<header class="header-contain">
+	<img src="<?php echo get_template_directory_uri();?>/assets/img/rockshop-web-logo.png" class="img-fluid logo-image mx-auto d-block" alt="The Rockshop Performing Arts Center" />
+	<div id="video-container">
+        <video autoplay loop class="fillWidth d-none d-sm-none d-md-block">
+            <source src="<?php echo get_template_directory_uri(); ?>/assets/img/rockshop-vid.mp4" type="video/mp4"/>
+            <source src="<?php echo get_template_directory_uri(); ?>/assets/img/rockshop-vid.ogg" type="video/ogg"/>
+            <source src="<?php echo get_template_directory_uri(); ?>/assets/img/rockshop-vid.webm" type="video/webm"/>
+            Your browser does not support the video tag. I suggest you upgrade your browser.
+        </video>
+	</div>
 </header>
 		
