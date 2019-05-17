df5939c3c5721fa6d60f44e21bc10f96bb57d455
Ben Kaminski <ben@benkaminski.com>
2019-05-17 15:38:41 -0400

    added H2 to title on page.php!

--- page.php
+++ page.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
 	<div class="container interior-wrapper pt-3 pb-3">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php the_title(); ?>
+			<h1><?php the_title(); ?></h1>
 
 			<?php the_content(); ?>
-
-
 
 		<?php endwhile; else : ?>
 			<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
