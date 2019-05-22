786596c5fe782ca249ce05ca3cbf0b83be5db325
Ben Kaminski <ben@benkaminski.com>
2019-05-22 10:56:00 -0400

    added 404.php

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+	<div class="container interior-wrapper pt-3 pb-3">
+		<div class="alert alert-danger">
+			<h1>404: Page not found</h1>
+		</div>
+		<p>The page you are looking for has either moved or never existed on https://bnbrockshop.com</p>
+		<p>Please choose an option from the menu above or try our search if you're looking for something in-particular</p>
+	</div>
+<?php get_footer(); ?>
