52413f11970aa96407213f3fb6c67cbaa3601365
Ben Kaminski <ben@benkaminski.com>
2019-05-24 12:28:52 -0400

    added validation to form inputs, blocked spec chars

--- assets/js/rockshopScripts.js
+++ assets/js/rockshopScripts.js
@@ -5,3 +5,15 @@ $(function() {
 	$(".main-event-desc").matchHeight();
 	$(".main-event-title").matchHeight();
 });
+$(document).ready(function(){
+  $("#s, #yourName, #yourSubject, #yourMessage, #quiz").keypress(function(e){
+    var keyCode = e.which;
+    // Not allow special 
+    if ( !( (keyCode >= 48 && keyCode <= 57) 
+      ||(keyCode >= 65 && keyCode <= 90) 
+      || (keyCode >= 97 && keyCode <= 122) ) 
+      && keyCode != 8 && keyCode != 32) {
+      e.preventDefault();
+    }
+  });
+});
