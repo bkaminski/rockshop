616f3fb4fd092d5f622b487c67f8ac95fc934aaf
Ben Kaminski <ben@benkaminski.com>
2019-05-31 11:41:10 -0400

    added jpg back to rockshop logo

--- parts/page-header.php
+++ parts/page-header.php
@@ -1,6 +1,6 @@
 <header class="header-contain">
 	<a href="<?php echo get_home_url(); ?>">
-		<img src="<?php echo get_template_directory_uri();?>/assets/img/rockshop-web-logo.webp" class="img-fluid logo-image mx-auto d-block" alt="The Rockshop Performing Arts Center" />
+		<img src="<?php echo get_template_directory_uri();?>/assets/img/rockshop-web-logo.png" class="img-fluid logo-image mx-auto d-block" alt="The Rockshop Performing Arts Center" />
 	</a>
 </header>
 		
